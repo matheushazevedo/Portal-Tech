8c193a6c6adc8a01dc3af6362634d6b20edae9d4
Matheus Azevedo <matheusazevedo1968@gmail.com>
2022-10-31 16:05:05 -0300

    new exercise "dateTime"

---
+++ Python/dateTime.py
@@ -0,0 +1,15 @@
+import datetime
+
+currentDateTime = datetime.datetime.now()
+date = currentDateTime.date()
+year = date.strftime("%Y")
+
+name = str(input('Enter your complete name: ')).title().strip().split()
+while True:   
+    try:
+        yearOfBirth = int(input(f'{name[0]}, enter the year of your birth: '))
+    except ValueError:
+        print('ERRO: Please enter a valid year and try again.')
+    else:
+        print(f'{name[0]}, this year you will be {int(year) - yearOfBirth} years old')
+        break
